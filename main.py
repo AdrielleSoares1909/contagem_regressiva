4098e8460dcaf8c21dd90bd2e0eca51eb2894a52
Adrielle Cristian <dricalove1@hotmail.com>
2026-02-23 11:09:03 -0300

    Contagem Regressiva

---
+++ main.py
@@ -0,0 +1,23 @@
+""" 
+    Contagem regressiva:
+
+ Mostre uma contagem de 10 até 0 usando loop."""
+
+
+
+valor_inicial = 10
+
+for i in range(0,11):
+    print(valor_inicial)
+    valor_inicial -= 1
+
+print("Fim do Programa")
+
+
+
+"""
+     Mais um projeto:
+     Dificuldades: usar loop, for, in pouco mais complicado.
+
+
+"""
